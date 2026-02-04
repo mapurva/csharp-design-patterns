045038b5fc658371597b6d86792dbf696622ecad
Apurva Mehta <apurvamehta2015@gmail.com>
2026-02-04 09:59:50 +0530

    Add Singleton pattern in C#

---
+++ 01-Creational/Singleton/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Try to get two instances
+        Singleton s1 = Singleton.GetInstance();
+        Singleton s2 = Singleton.GetInstance();
+
+        s1.ShowMessage();
+
+        // Check if both references point to same object
+        if (s1 == s2)
+        {
+            Console.WriteLine("Both are same instance");
+        }
+        else
+        {
+            Console.WriteLine("Different instances");
+        }
+    }
+}
