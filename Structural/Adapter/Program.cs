b88ef19592ffbb9bf4af16cc86d1f90875f34824
Apurva Mehta <apurvamehta2015@gmail.com>
2026-02-10 09:59:10 +0530

    Add client code demonstrating Adapter usage

--- Structural/Adapter/Program.cs
+++ Structural/Adapter/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdapterPattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IPaymentProcessor paymentProcessor = new BankAdapter();
+
+        paymentProcessor.Pay(1500.50);
+    }
+}
