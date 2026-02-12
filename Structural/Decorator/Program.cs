c9b0c5e069463c7a396827c2f85d88684178f19c
Apurva Mehta <apurvamehta2015@gmail.com>
2026-02-12 08:54:44 +0530

    Add client code demonstrating Decorator usage

--- Structural/Decorator/Program.cs
+++ Structural/Decorator/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DecoratorPattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ICoffee coffee = new SimpleCoffee();
+
+        coffee = new MilkDecorator(coffee);
+        coffee = new SugarDecorator(coffee);
+
+        Console.WriteLine(coffee.GetDescription());
+        Console.WriteLine("Total Cost: " + coffee.GetCost());
+    }
+}
