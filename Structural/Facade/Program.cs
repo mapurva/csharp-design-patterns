7fe1e0bb0872531a9692b35925c14ece52477b7d
Apurva Mehta <apurvamehta2015@gmail.com>
2026-02-14 10:15:08 +0530

    Add client code demonstrating Facade usage

--- Structural/Facade/Program.cs
+++ Structural/Facade/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FacadePattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HomeTheaterFacade theater = new HomeTheaterFacade();
+
+        theater.WatchMovie();
+        theater.EndMovie();
+    }
+}
