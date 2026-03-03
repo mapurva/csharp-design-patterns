6d27dc5ebad65c25d49b3130260bb27cc3862601
Apurva Mehta <apurvamehta2015@gmail.com>
2026-03-03 09:35:01 +0530

    Add client demonstrating Observer notifications

--- Behavioral/Observer/Program.cs
+++ Behavioral/Observer/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ObserverPattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Stock stock = new Stock();
+
+        Investor investor1 = new Investor("Apurva");
+        Investor investor2 = new Investor("Rahul");
+
+        stock.Attach(investor1);
+        stock.Attach(investor2);
+
+        stock.SetPrice(1000);
+        stock.SetPrice(1200);
+
+        stock.Detach(investor1);
+
+        stock.SetPrice(1500);
+    }
+}
