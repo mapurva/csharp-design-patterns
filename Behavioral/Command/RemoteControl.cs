e346af58ab83cca0ed5d69fde2d7210e24e65c45
Apurva Mehta <apurvamehta2015@gmail.com>
2026-03-05 10:37:43 +0530

    Add RemoteControl invoker

---
+++ Behavioral/Command/RemoteControl.cs
@@ -0,0 +1,17 @@
+namespace CommandPattern
+{
+    public class RemoteControl
+    {
+        private ICommand _command;
+
+        public void SetCommand(ICommand command)
+        {
+            _command = command;
+        }
+
+        public void PressButton()
+        {
+            _command.Execute();
+        }
+    }
+}
