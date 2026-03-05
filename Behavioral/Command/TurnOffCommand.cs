54339c75b026c3e07d04e9fcf4f856c7a0baa687
Apurva Mehta <apurvamehta2015@gmail.com>
2026-03-05 10:37:12 +0530

    Add TurnOnCommand and TurnOffCommand implementations

---
+++ Behavioral/Command/TurnOffCommand.cs
@@ -0,0 +1,17 @@
+namespace CommandPattern
+{
+    public class TurnOffCommand : ICommand
+    {
+        private Light _light;
+
+        public TurnOffCommand(Light light)
+        {
+            _light = light;
+        }
+
+        public void Execute()
+        {
+            _light.TurnOff();
+        }
+    }
+}
---
+++ Behavioral/Command/TurnOnCommand.cs
@@ -0,0 +1,17 @@
+namespace CommandPattern
+{
+    public class TurnOnCommand : ICommand
+    {
+        private Light _light;
+
+        public TurnOnCommand(Light light)
+        {
+            _light = light;
+        }
+
+        public void Execute()
+        {
+            _light.TurnOn();
+        }
+    }
+}
