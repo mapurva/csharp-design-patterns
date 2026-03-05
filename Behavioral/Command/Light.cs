1f723ad75573726737559e20c8e7fe31d61a2764
Apurva Mehta <apurvamehta2015@gmail.com>
2026-03-05 10:36:38 +0530

    Add Light receiver class

---
+++ Behavioral/Command/Light.cs
@@ -0,0 +1,15 @@
+namespace CommandPattern
+{
+    public class Light
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("Light is ON");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Light is OFF");
+        }
+    }
+}
