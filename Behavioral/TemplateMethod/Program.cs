688ab6a6acba9d40c092705dbd2d4162f32cbe67
Apurva Mehta <apurvamehta2015@gmail.com>
2026-03-09 10:32:36 +0530

    Add client demonstrating Template Method pattern

--- Behavioral/TemplateMethod/Program.cs
+++ Behavioral/TemplateMethod/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TemplateMethodPattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataProcessor csv = new CSVProcessor();
+        csv.Process();
+
+        Console.WriteLine();
+
+        DataProcessor json = new JSONProcessor();
+        json.Process();
+    }
+}
