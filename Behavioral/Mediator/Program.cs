31ac20fbfd57839b8eb28649550771a1441abf17
Apurva Mehta <apurvamehta2015@gmail.com>
2026-03-11 09:24:05 +0530

    Add client demonstrating Mediator pattern

--- Behavioral/Mediator/Program.cs
+++ Behavioral/Mediator/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MediatorPattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ChatRoomMediator chatRoom = new ChatRoomMediator();
+
+        User user1 = new ChatUser(chatRoom, "Apurva");
+        User user2 = new ChatUser(chatRoom, "Rahul");
+        User user3 = new ChatUser(chatRoom, "Neha");
+
+        chatRoom.AddUser(user1);
+        chatRoom.AddUser(user2);
+        chatRoom.AddUser(user3);
+
+        user1.Send("Hello everyone!");
+    }
+}
