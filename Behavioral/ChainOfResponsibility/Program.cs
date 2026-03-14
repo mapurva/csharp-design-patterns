f9bfd78d31c1d89186c39230e146101646e335a3
Apurva Mehta <apurvamehta2015@gmail.com>
2026-03-14 11:50:39 +0530

    Add client demonstrating Chain of Responsibility pattern

--- Behavioral/ChainOfResponsibility/Program.cs
+++ Behavioral/ChainOfResponsibility/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ChainOfResponsibilityPattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SupportHandler basic = new BasicSupport();
+        SupportHandler supervisor = new Supervisor();
+        SupportHandler manager = new Manager();
+
+        basic.SetNext(supervisor);
+        supervisor.SetNext(manager);
+
+        basic.HandleRequest(1);
+        basic.HandleRequest(2);
+        basic.HandleRequest(3);
+        basic.HandleRequest(4);
+    }
+}
