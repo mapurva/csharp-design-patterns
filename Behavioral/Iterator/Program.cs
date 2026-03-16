7df9f186ac1e9b35484cec5eb2949be144e29662
Apurva Mehta <apurvamehta2015@gmail.com>
2026-03-16 14:55:55 +0530

    Add client demonstrating Iterator pattern

--- Behavioral/Iterator/Program.cs
+++ Behavioral/Iterator/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using IteratorPattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BookCollection collection = new BookCollection();
+
+        collection.AddBook("Design Patterns");
+        collection.AddBook("Clean Code");
+        collection.AddBook("Refactoring");
+
+        IIterator iterator = collection.CreateIterator();
+
+        while (iterator.HasNext())
+        {
+            Console.WriteLine(iterator.Next());
+        }
+    }
+}
