48822ed9f6a55f9baa721f8ee22d83079b0b6070
Apurva Mehta <apurvamehta2015@gmail.com>
2026-04-02 10:02:04 +0530

    Add client demonstrating Visitor pattern

--- Behavioral/Visitor/Program.cs
+++ Behavioral/Visitor/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using VisitorPattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<IShape> shapes = new List<IShape>
+        {
+            new Circle(5),
+            new Rectangle(4, 6)
+        };
+
+        IVisitor areaVisitor = new AreaVisitor();
+        IVisitor drawVisitor = new DrawVisitor();
+
+        Console.WriteLine("Calculating Areas:");
+        foreach (var shape in shapes)
+        {
+            shape.Accept(areaVisitor);
+        }
+
+        Console.WriteLine("\nDrawing Shapes:");
+        foreach (var shape in shapes)
+        {
+            shape.Accept(drawVisitor);
+        }
+    }
+}
