e7dd11d20596ad76a89dea91033219449fc29611
Apurva Mehta <apurvamehta2015@gmail.com>
2026-04-04 10:58:02 +0530

    Add SubtractExpression

---
+++ Behavioral/Interpreter/SubtractExpression.cs
@@ -0,0 +1,19 @@
+namespace InterpreterPattern
+{
+    public class SubtractExpression : IExpression
+    {
+        private IExpression _left;
+        private IExpression _right;
+
+        public SubtractExpression(IExpression left, IExpression right)
+        {
+            _left = left;
+            _right = right;
+        }
+
+        public int Interpret()
+        {
+            return _left.Interpret() - _right.Interpret();
+        }
+    }
+}
