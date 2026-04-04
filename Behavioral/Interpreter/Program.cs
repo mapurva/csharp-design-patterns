892c0b8875ba129461bdc7553a54205c0467e7c4
Apurva Mehta <apurvamehta2015@gmail.com>
2026-04-04 10:58:33 +0530

    Add client demonstrating Interpreter pattern

--- Behavioral/Interpreter/Program.cs
+++ Behavioral/Interpreter/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using InterpreterPattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Representing: (5 + 3) - 2
+
+        IExpression expression = new SubtractExpression(
+            new AddExpression(
+                new Number(5),
+                new Number(3)
+            ),
+            new Number(2)
+        );
+
+        int result = expression.Interpret();
+
+        Console.WriteLine("Result: " + result);
+    }
+}
